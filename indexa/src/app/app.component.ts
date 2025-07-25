d4409884d1455e892c460b299568ff03fa03e762
Lekito <alexlekito075@gmail.com>
2025-07-25 10:47:25 -0300

    feat: create `removerAcentos`

--- indexa/src/app/app.component.ts
+++ indexa/src/app/app.component.ts
@@ -36,15 +36,64 @@ export class AppComponent {
 
   filtroPorTexto: string = '';
 
+  private removerAcentos(s: string): string {
+    let map: { [key: string]: string } = {
+      â: 'a',
+      Â: 'A',
+      à: 'a',
+      À: 'A',
+      á: 'a',
+      Á: 'A',
+      ã: 'a',
+      Ã: 'A',
+      ê: 'e',
+      Ê: 'E',
+      è: 'e',
+      È: 'E',
+      é: 'e',
+      É: 'E',
+      î: 'i',
+      Î: 'I',
+      ì: 'i',
+      Ì: 'I',
+      í: 'i',
+      Í: 'I',
+      õ: 'o',
+      Õ: 'O',
+      ô: 'o',
+      Ô: 'O',
+      ò: 'o',
+      Ò: 'O',
+      ó: 'o',
+      Ó: 'O',
+      ü: 'u',
+      Ü: 'U',
+      û: 'u',
+      Û: 'U',
+      ú: 'u',
+      Ú: 'U',
+      ù: 'u',
+      Ù: 'U',
+      ç: 'c',
+      Ç: 'C',
+    };
+
+    return s
+      .replace(/[\W\[\] ]/g, function (a: string): string {
+        return map[a] || a;
+      })
+      .toLowerCase();
+  }
+
   filtrarContatosPorTexto(): Contato[] {
     if (!this.filtroPorTexto) {
       return this.contatos;
     }
 
     return this.contatos.filter((contato) => {
-      return contato.nome
-        .toLowerCase()
-        .includes(this.filtroPorTexto.toLowerCase());
+      return this.removerAcentos(contato.nome).includes(
+        this.filtroPorTexto.toLowerCase()
+      );
     });
   }
 
