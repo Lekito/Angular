04901b9b2ad6ce0eb2480c0a1fc4dcb6f66899af
Alex Ribeiro Alves <alex.alves@totvs.com.br>
2026-02-06 13:10:06 -0300

    feat: add rotas, add icons, add id number

--- indexa/src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ indexa/src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { Contato } from '../../componentes/contato/contato';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
   imports: [
     ContainerComponent,
+    RouterLink
   ],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css'
