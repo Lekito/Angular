a665e7abe6e6c426da511e71a12907ad116263d1
Lekito <alexlekito075@gmail.com>
2026-02-15 09:28:05 -0300

    feat: add photo

--- indexa/src/app/componentes/contato/contato.ts
+++ indexa/src/app/componentes/contato/contato.ts
@@ -1,6 +1,7 @@
 export interface Contato {
   id: number;
   nome: string;
+  avatar: string | ArrayBuffer,
   telefone: string;
   email: string;
   aniversario?: string; // interrogação aqui serve para deixar esse tipo opcional.
